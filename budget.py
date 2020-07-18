a3f4c2d74c139cbbf1f2d681b8de04264f712324
Manuel Guevara <manuel117g@gmail.com>
2020-07-18 14:41:14 -0400

    Add files via upload

--- budget.py
+++ budget.py
@@ -1,7 +1,22 @@
 class Category:
-  a
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
 
+    def deposit(self, amount, description=""):
+        print(self.name + amount)
 
+    def withdraw(self, amount, description=""):
+        print(self.name + amount)
+
+    def get_balance(self):
+        print("balance")
+
+    def transfer(self):
+        print("transfer")
+
+    def check_founds(self):
+        print("check_founds")
 
 
 def create_spend_chart(categories):
