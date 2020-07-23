c854ba9b09e9557caa6811bba7f0a015f520084f
MagcDEV <manuel117g@gmail.com>
2020-07-23 11:08:58 -0400

    final final final

--- budget.py
+++ budget.py
@@ -105,7 +105,7 @@ def create_spend_chart(categories):
         count += 1
     return chart
  
-food = Category("Food")
+""" food = Category("Food")
 entertainment = Category("Entertainment")
 business = Category("Business")
 food.deposit(900, "deposit")
@@ -118,4 +118,4 @@ print(create_spend_chart([business, food, entertainment]))
 
 expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10| o  o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
         
-print(expected)
+print(expected) """
